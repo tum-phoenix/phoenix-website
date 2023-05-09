f9cb221fff83429743c24f7ba8c5cae6494ca6e2
Kai <84288341+kaieberl@users.noreply.github.com>
2023-05-09 20:57:14 +0200

    Contact page placeholder (#2) (#3)
    * landing page
    
    * style fix
    
    * images, links, style
    
    * header image, logo spacing, responsive font sizes
    
    * style improvements
    
    * fix dark bg and goals
    
    * style improvements
    
    * footer
    
    * add drive page, some improvements to navigation bar, home and footer
    
    * use positionsList instead of FaqList for positions
    
    * add drive and flight pages, improve design of apply page
    
    * add Bosch FMC page, style improvements
    
    * add preliminary team page
    
    * improve style of apply page
    
    * add placeholder for contact page
    
    ---------
    
    
    Co-authored-by: Kai <>
    
    Co-authored-by: Friendly-Banana <69007475+Friendly-Banana@users.noreply.github.com>

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,13 @@
+function Contact() {
+  return (
+    <div className="contactpageContainer">
+      <h1>Contact us</h1>
+      <p>
+        Feel free to contact us! We are always open to new project ideas,
+        collaborations, and feedback - send us a message!
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
