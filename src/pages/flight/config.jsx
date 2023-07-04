e582be029d4a35d40697b3c2977b80e45da89121
Kai <>
2023-07-04 16:27:43 +0200

    add slider gallery and sample content

--- src/pages/flight/config.jsx
+++ src/pages/flight/config.jsx
@@ -1,12 +1,13 @@
 // images
-import gsImg from "./img/goldensnitch.jpg";
+import gsImg from "./img/team.jpg";
 
 const projectsConfig = {
   golden_snitch: {
     id: "goldensnitch",
     title: "Golden Snitch",
     image: gsImg,
-    text: "test",
+    text: "Ähnlich wie der 'Schnatz' (engl. Golden Snitch) aus Harry Potter soll diese Drohne in einem Spiel eingesetzt werden, bei der die Spieler die Drohne fangen müssen. Dabei haben die Spieler AR-Brillen auf, sodass die Drohne als beliebiges Objekt angezeigt werden kann, eben auch als magischer fliegender Schnatz. Hierfür verfügt die Drohne über eine autonome Steuerung, die über Kameras ihre Umgebung wahrnimmt und mithilfe von Machine Learning Objekte und die Position der Spieler erkennt. Durch LED-Bänder an der Drohne können auch visuelle Effekte zusätzlich zur AR-Welt einprogrammiert werden. Zum Schutz der Spieler und für bessere Crash-Resistenz ist die Drohne von einem stabilen Carbonfaser-Käfig umgeben.\n" +
+        "Für das Projekt kooperieren wir mit dem Lehrstuhl für Augmented Reality und mit dem Lehrstuhl für Flugsystemdynamik.",
   },
   tarot: {
     id: "tarot",
