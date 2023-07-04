8747df061d926e96ef4a98e1e836028559821467
GXY-WORLDPEACE <104737807+GXY-WORLDPEACE@users.noreply.github.com>
2023-07-04 18:12:33 +0200

    add content to team page

--- src/pages/flight/config.jsx
+++ src/pages/flight/config.jsx
@@ -1,19 +1,33 @@
 // images
-import gsImg from "./img/team.jpg";
+import gsImg from "./img/golden_snitch.jpeg";
+import gstsImg from "./img/gs_technical_sketch.jpg";
+import gseImg from "./img/gs_electronics.jpg";
+import tarotImg from "./img/tarot.png";
 
 const projectsConfig = {
   golden_snitch: {
     id: "goldensnitch",
     title: "Golden Snitch",
     image: gsImg,
-    text: "Ähnlich wie der 'Schnatz' (engl. Golden Snitch) aus Harry Potter soll diese Drohne in einem Spiel eingesetzt werden, bei der die Spieler die Drohne fangen müssen. Dabei haben die Spieler AR-Brillen auf, sodass die Drohne als beliebiges Objekt angezeigt werden kann, eben auch als magischer fliegender Schnatz. Hierfür verfügt die Drohne über eine autonome Steuerung, die über Kameras ihre Umgebung wahrnimmt und mithilfe von Machine Learning Objekte und die Position der Spieler erkennt. Durch LED-Bänder an der Drohne können auch visuelle Effekte zusätzlich zur AR-Welt einprogrammiert werden. Zum Schutz der Spieler und für bessere Crash-Resistenz ist die Drohne von einem stabilen Carbonfaser-Käfig umgeben.\n" +
-        "Für das Projekt kooperieren wir mit dem Lehrstuhl für Augmented Reality und mit dem Lehrstuhl für Flugsystemdynamik.",
+    text: "",
+  },
+  gs_technical_sketch: {
+    id: "gstechnicalsketch",
+    title: "gs_Technical_Sketch",
+    image: gstsImg,
+    text: "",
+  },
+  gs_electronics: {
+    id: "gselectronics",
+    title: "gs_electronics",
+    image: gseImg,
+    text: "",
   },
   tarot: {
     id: "tarot",
     title: "Tarot",
-    image: gsImg,
-    text: "test2",
+    image: tarotImg,
+    text: "",
   },
 };
 
