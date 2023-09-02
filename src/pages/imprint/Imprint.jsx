1a6f1c9dafed76e7d5792840ec6120b688274e4c
Kai <>
2023-09-02 10:08:45 +0200

    add imprint

---
+++ src/pages/imprint/Imprint.jsx
@@ -0,0 +1,50 @@
+import './style.css';
+
+function Imprint() {
+  return (
+    <div>
+      <h1>Imprint</h1>
+      <h2>Address</h2>
+      <p>
+        Phoenix Robotics e.V.<br />
+        Technische Universität München<br />
+        Boltzmannstraße 15<br />
+        85748 Garching bei München<br />
+        Am Lehrstuhl für Regelungstechnik<br />
+        Email: <a href="mailto:kontakt@tum-phoenix.de">kontakt.at.tum-phoenix.de</a>
+      </p>
+      <h2>Represented by</h2>
+      <p>
+        The club is represented by the board of directors, which consists of the
+        following members:
+      </p>
+      <ul>
+        <li>Denis Gretz</li>
+        <li>Chenyu Meng</li>
+      </ul>
+      <h2>Register entry</h2>
+      <p>
+        TBD
+      </p>
+      <h2>Responsible for the content according to § 55 Abs. 2 RStV</h2>
+      <p>
+        Kai Eberl<br />
+      </p>
+      <h2>Disclaimers</h2>
+      <h3>Liability for contents</h3>
+      <p>
+        Although the contents of our pages have been created with the utmost
+        care, we cannot assume any liability for the correctness, completeness
+        and topicality of the contents.
+      </p>
+      <h3>Liability for links</h3>
+      <p>
+        Our website contains links to external websites of third parties, on whose
+        contents we have no influence. Therefore, we cannot assume any liability
+        for these external contents.
+      </p>
+    </div>
+  );
+}
+
+export default Imprint;
